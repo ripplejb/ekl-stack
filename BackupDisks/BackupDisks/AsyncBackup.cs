8511aacf65df0a0ac4ba2cf5678e19b19e43f163
ripalbarot <ripplejb@hotmail.com>
2022-02-14 23:49:33 -0600

    Added a sample project

---
+++ BackupDisks/BackupDisks/AsyncBackup.cs
@@ -0,0 +1,115 @@
+using System.Threading.Channels;
+using Serilog;
+
+namespace BackupDisks;
+
+public class AsyncBackup
+{
+    private readonly Channel<string[]> _fileQueue = Channel.CreateUnbounded<string[]>();
+    private readonly CancellationTokenSource _cancellationTokenSource = new();
+    private readonly ILogger _logger;
+
+    public AsyncBackup(ILogger logger)
+    {
+        _logger = logger;
+    }
+
+    public void CancelCopy()
+    {
+        _cancellationTokenSource.Cancel();
+    }
+    
+    public async Task Copy(string src, string dst)
+    {
+        _logger.Information("Start channel");
+        await StartChannel(src, dst, _cancellationTokenSource.Token);
+        await ReadChannel(_cancellationTokenSource.Token);
+    }
+
+    private async Task StartChannel(string src, string dst, CancellationToken cancellationToken)
+    {
+        var stack = new Stack<string>();
+        if (!Directory.Exists(src) || !Directory.Exists(dst))
+        {
+            _logger.Error(new Exception("Invalid source or destination folder"), "Error copying folder. Make sure folders exists");
+            return;
+        }
+        stack.Push(src);
+        while (stack.TryPop(out var dir))
+        {
+            CreateFolder(src, dst, dir);
+            var dirs = Directory.GetDirectories(dir);
+            PushToStack(stack, dirs);
+            await WriteFilesToChannel(src, dst, dir, cancellationToken);
+        }
+        
+    }
+
+    private async Task ReadChannel(CancellationToken cancellationToken)
+    {
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            var file = await _fileQueue.Reader.ReadAsync(cancellationToken);
+            await Task.Run(() =>
+            {
+                if (cancellationToken.IsCancellationRequested) return;
+                _logger.Information("Start copying file {File}", file[0]);
+                try
+                {
+                    File.Copy(file[0], file[1]);
+                    _logger.Information("Finished copying file {File}", file[0]);
+                }
+                catch (Exception e)
+                {
+                    _logger.Error(e, "Error copying file {File}", file[0]);
+                }
+            });
+            if (cancellationToken.IsCancellationRequested) break;
+        }
+    }
+    
+    private async Task WriteFilesToChannel(string src, string dst, string folder, CancellationToken cancellationToken)
+    {
+        var files = Directory.GetFiles(folder);
+        var newFolder = GetNewFolderName(src, dst, folder); 
+        foreach (var file in files)
+        {
+            await _fileQueue.Writer.WriteAsync(new[] {file, Path.Combine(newFolder, Path.GetFileName(file))}, cancellationToken);
+        }
+    }
+    
+    private void CreateFolder(string src, string dst, string newFolderPath)
+    {
+        var folderName = GetNewFolderName(src, dst, newFolderPath);
+        _logger.Information("Create folder {FolderName}", folderName);
+        try
+        {
+            Directory.CreateDirectory(folderName);
+        }
+        catch (Exception e)
+        {
+            _logger.Error(e, "Error creating folder {FolderName}", folderName);
+            throw;
+        }
+        
+    }
+
+    private string GetNewFolderName(string src, string dst, string newFolderPath)
+    {
+        var newFolder = Path.Combine(dst, new DirectoryInfo(src).Name);
+        if (!src.Equals(newFolderPath))
+        {
+            newFolder = Path.Combine(newFolder, newFolderPath.Substring(src.Length + 1));
+        }
+
+        return newFolder;
+    }
+    
+    private void PushToStack(Stack<string> stack, string[] list)
+    {
+        foreach (var s in list)
+        {
+            stack.Push(s);
+        }
+    }
+}
---
+++ BackupDisks/BackupDisks/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BackupDisks;
+using Serilog;
+using Serilog.Formatting.Json;
+using Serilog.Sinks.RabbitMQ;
+
+var loggerConfig = new LoggerConfiguration()
+    .WriteTo.RabbitMQ((clientConfig, sinksConfig) =>
+    {
+        clientConfig.RouteKey = "backup-rk";
+        clientConfig.Hostnames.Add("localhost");
+        clientConfig.Exchange = "app-logging";
+        clientConfig.ExchangeType = "direct";
+        clientConfig.Username = "guest";
+        clientConfig.Password = "guest";
+        clientConfig.Port = 5672;
+        clientConfig.DeliveryMode = RabbitMQDeliveryMode.Durable;
+
+        sinksConfig.TextFormatter = new JsonFormatter();
+    })
+    .Enrich.WithProperty("Environment", "My Local PC")
+    .Enrich.WithThreadId();
+
+var asyncBackup = new AsyncBackup(loggerConfig.CreateLogger());
+
+asyncBackup.Copy("/home/ripalbarot/WebstormProjects", "/home/ripalbarot/Temp");
+
+Console.WriteLine("Type 'Quit' to exit.");
+var inStr = string.Empty;
+while (inStr is not "Quit")
+{
+    inStr = Console.ReadLine();
+}
+asyncBackup.CancelCopy();
+Thread.Sleep(60000);
+
